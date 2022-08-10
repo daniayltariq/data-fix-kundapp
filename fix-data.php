d56023425b2eade53f465dafaf70bf186ba0d285
WAHHAB <daniyaldottariq@gmail.com>
2022-08-10 11:09:26 +0500

    fix

--- fix-data.php
+++ fix-data.php
@@ -44,7 +44,7 @@ ini_set('memory_limit', -1);
           die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "SELECT id, living_type_string, living_type, address, post_number FROM `customer_contacts_backup_4` where living_type IN (124959,124960,145340) and address is not null and id < 200";
+        $sql = "SELECT id, living_type_string, living_type, address, post_number FROM `customer_contacts_backup_4` where living_type IN (124959,124960,145340) and address is not null and id < 20";
         $result = $conn->query($sql);
 
         //3.4M
@@ -89,7 +89,7 @@ ini_set('memory_limit', -1);
 		            	$address = $clean_address[0];
 
                         
-                        $sql = "Update customer_contacts set living_type = '".$living_type."', living_type_string = '". $living_type_string ."', nix = '". $nix ."' where address = '". $address . "%' and address_type = 1321 and living_type is null";
+                        echo $sql = "Update customer_contacts set living_type = '".$living_type."', living_type_string = '". $living_type_string ."', nix = '". $nix ."' where address = '". $address . "%' and address_type = 1321 and living_type is null";
                         // die();
 
                         $conn->query($sql);
